3083ee1e7585af7bef8cf4af87ede2554ca9eee2
alirezamajdi <majdialireza01@gmail.com>
2025-12-05 20:41:08 +0330

    initial

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
+
+function App() {
+   const [message, setMessage] = useState('');
+
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
+
+   return (
+      <>
+         <p className=" font-bold text-center">{message}</p>
+         <Button>Click me</Button>
+      </>
+   );
+}
+
+export default App;
